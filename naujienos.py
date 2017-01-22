a444faa3629a396ea283b8200795e8a69a65248a
reddogLT <j.malickas@gmail.com>
2017-01-22 18:46:10 +0200

    added sceleton

--- naujienos.py
+++ naujienos.py
@@ -1,1 +1,9 @@
-import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def get_newa():
+    return "no news still!"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
