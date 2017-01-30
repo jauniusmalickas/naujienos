e9a25a4c14724c42aa70b48715ef641580a68b59
reddogLT <j.malickas@gmail.com>
2017-01-30 19:45:32 +0200

    Without request

--- naujienos.py
+++ naujienos.py
@@ -1,19 +1,19 @@
 import feedparser
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-            'fox': 'http://feeds.foxnews.com/foxnews/latest'}
+            'fox': 'http://feeds.foxnews.com/foxnews/most-popular'}
 
 @app.route("/")
 @app.route("/<provider>")
 
 def get_news(provider='bbc'):
     feed = feedparser.parse(RSS_FEEDS[provider])
-    #first_article = feed['entries'][0]
+   
     articles=feed['entries']
     if articles:
         return render_template('home.html', provider=provider, articles=articles)
