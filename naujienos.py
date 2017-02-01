5e805a552c43c6b46c5044429565e16545378784
reddogLT <j.malickas@gmail.com>
2017-02-01 21:31:15 +0200

    added home function

--- naujienos.py
+++ naujienos.py
@@ -7,33 +7,46 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+DEFAULTS = {'provider':'bbc', 'city':'Vilnius, LT'}
+
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
             'fox': 'http://feeds.foxnews.com/foxnews/most-popular'}
 
 #using GET method to get feeds provider from URL like ?provider=bbc
 @app.route("/")
-def get_news():
-    query = request.args.get('provider')
-    if not query or query.lower() not in RSS_FEEDS:
-        provider ='bbc'
-    else:
-        provider = query.lower()
+def home():
+    provider = request.args.get('provider')
+    if not provider or provider.lower() not in RSS_FEEDS:
+        provider =DEFAULTS['provider']
 
-    weather = get_weather("London,UK")
-    feed = feedparser.parse(RSS_FEEDS[provider])
-    articles=feed['entries']
+    articles = get_news(provider.lower())
+
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    
+    weather = get_weather(city)
+
     if articles:
         return render_template('home.html', provider=provider, articles=articles, weather=weather)
     else:
         return "no news still!"
 
-def get_weather(query):
+
+#get_news procedure based on provider paramater
+def get_news(provider):
+    
+    feed = feedparser.parse(RSS_FEEDS[provider])
+    return feed['entries']
+    
+#procedure to retrieve weather information with query parameter as city and country
+def get_weather(city):
     args = {"q": "", "units": "metric", "appid": "bbaffd18fad8101c96023d9a4c248018"}
     base_url = "http://api.openweathermap.org/data/2.5/weather?{}"
        
-    query = urllib.parse.quote(query) 
-    args.update(q=query)
+    city = urllib.parse.quote(city) 
+    args.update(q=city)
 
     url = base_url.format(urllib.parse.urlencode(args))
 
